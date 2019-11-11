c37fb96f6515bd07d855f8f78e43a1a798cd185d
itskaramaman <45313619+itskaramaman@users.noreply.github.com>
2019-11-11 12:51:15 +0530

    database url corrected

--- main/__init__.py
+++ main/__init__.py
@@ -5,7 +5,7 @@ from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://enilcqpnnvivrd:9e1f93cf1c67489a93a2b856d48c1b84a78d5338992d29d5d984d309bcb28812@ec2-54-197-238-238.compute-1.amazonaws.com:5432/d7h77110l6d2fi'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secret_shhh'
 
